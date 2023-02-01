cb0ba38df69bd358cb009add18fc64a0f6b7eea6
Axell <sobolev89@bk.ru>
2023-02-01 11:30:13 +0300

    First commit

--- project_0/game.py
+++ project_0/game.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import numpy as np
 
 number = np.random.randint(1, 101) # загадываем число
@@ -8,6 +9,19 @@ while True:
     count += 1
     predict_number = int(input("Угадай число от 1 до 100"))
 
+=======
+""" Игра угадай число """
+
+import numpy as np
+
+number =  np.random.randint(1, 101) # загадываем число
+
+count = 0 # колличество попыток
+
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100:"))
+>>>>>>> 15de14d (First commit)
     if predict_number > number:
         print("Число должно быть меньше!")
 
@@ -17,3 +31,7 @@ while True:
     else:
         print(f"Вы угадали число! Это число = {number}, за {count} попыток")
         break # конец игры, выход из цикла
+<<<<<<< HEAD
+=======
+    
+>>>>>>> 15de14d (First commit)
--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+=======
+"""Игра угадай число.
+Компьютер сам загадывает и угадывает число
+"""
+
+>>>>>>> 15de14d (First commit)
 import numpy as np
 
 def random_predict(number:int=1) -> int:
@@ -9,7 +16,11 @@ def random_predict(number:int=1) -> int:
     Returns:
         int: Число попыток
     """
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 15de14d (First commit)
     count = 0
 
     while True:
@@ -18,8 +29,14 @@ def random_predict(number:int=1) -> int:
         if number == predict_number:
             break # выход из цикла, если угадали
     return(count)
+<<<<<<< HEAD
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+=======
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+>>>>>>> 15de14d (First commit)
 
     Args:
         random_predict ([type]): функция угадывания
@@ -29,7 +46,11 @@ def score_game(random_predict) -> int:
     """
 
     count_ls = [] # список для сохранения количества попыток
+<<<<<<< HEAD
     np.random.seed(1) # фиксируем сид для воcпроизводимости
+=======
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+>>>>>>> 15de14d (First commit)
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
 
     for number in random_array:
@@ -40,5 +61,9 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
+<<<<<<< HEAD
+=======
+# RUN
+>>>>>>> 15de14d (First commit)
 if __name__ == '__main__':
     score_game(random_predict)
